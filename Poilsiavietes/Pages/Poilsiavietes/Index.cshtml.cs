7776c0831a93d7a0a5e069762f783bfeb6af0bfe
gabjoc <128813024+gabjoc@users.noreply.github.com>
2023-12-23 01:02:19 +0200

    Veikiantis poil filtras

--- Poilsiavietes/Pages/Poilsiavietes/Index.cshtml.cs
+++ Poilsiavietes/Pages/Poilsiavietes/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,17 +19,37 @@ namespace Poilsiavietes.Pages.Poilsiavietes
             _context = context;
         }
 
-        public IList<Poilsiaviete> Poilsiaviete { get;set; } = default!;
+        public IList<Poilsiaviete> Poilsiaviete { get;set; } = new List<Poilsiaviete>();
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(DateOnly dateFrom, DateOnly dateTill)
         {
-            if (_context.Poilsiavietes != null)
+            if(dateFrom.Year != 1 &&  dateTill.Year != 1)
             {
-                Poilsiaviete = await _context.Poilsiavietes
+                if (_context.Poilsiavietes != null)
+                {
+                    Poilsiaviete = await FilterPoilsiavietes(dateFrom, dateTill);
+                }
+            }
+        }
+
+        private async Task<List<Poilsiaviete>> FilterPoilsiavietes(DateOnly dateFrom, DateOnly dateTill)
+        {
+            var Rezervacijos = await _context.Rezervacijos
+                .Where(r => r.Busena < 3)
+                .Where(r => !(r.Pabaiga < dateFrom || r.Pradzia > dateTill))
+                .ToListAsync();
+
+            var nefiltruotosPoilsiavietes = await _context.Poilsiavietes
                 .Include(p => p.FkIdNaudotojasNavigation)
                 .Include(p => p.FkKodasNavigation)
-                .Include(p => p.TipasNavigation).ToListAsync();
-            }
+                .Include(p => p.TipasNavigation)
+                .ToListAsync();
+
+            var filtruotosPoilsiavietes = nefiltruotosPoilsiavietes
+                .Where(p => !Rezervacijos.Any(r => r.FkIdPoilsiaviete == p.IdPoilsiaviete))
+                .ToList();
+
+            return filtruotosPoilsiavietes;
         }
     }
 }
