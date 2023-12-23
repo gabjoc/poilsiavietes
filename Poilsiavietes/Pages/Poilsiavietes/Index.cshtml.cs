1e0dc8706c1ce0a058af986da8fac19ff57fde40
gabjoc <128813024+gabjoc@users.noreply.github.com>
2023-12-23 22:46:38 +0200

    Zemelapio funkcija done

--- Poilsiavietes/Pages/Poilsiavietes/Index.cshtml.cs
+++ Poilsiavietes/Pages/Poilsiavietes/Index.cshtml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Poilsiavietes.Models;
 
 namespace Poilsiavietes.Pages.Poilsiavietes
@@ -19,13 +21,13 @@ namespace Poilsiavietes.Pages.Poilsiavietes
             _context = context;
         }
 
-        public IList<Poilsiaviete> Poilsiaviete { get;set; } = new List<Poilsiaviete>();
-
+        public IList<Poilsiaviete> Poilsiaviete { get; set; } = new List<Poilsiaviete>();
+       
         public async Task OnGetAsync(DateOnly dateFrom, DateOnly dateTill)
         {
-            if(dateFrom.Year != 1 &&  dateTill.Year != 1)
+            if (dateFrom.Year != 1 && dateTill.Year != 1)
             {
-                if(dateTill <= dateFrom)
+                if (dateTill <= dateFrom)
                 {
                     TempData["ErrorMessage"] = "Nurodytas laikotarpis neteisingas.";
                     return;
@@ -33,11 +35,40 @@ namespace Poilsiavietes.Pages.Poilsiavietes
                 if (_context.Poilsiavietes != null)
                 {
                     Poilsiaviete = await FilterPoilsiavietes(dateFrom, dateTill);
+                    List<Tuple<double, double, string>> locations = new List<Tuple<double, double, string>>();
+
+                    string apiKey = Environment.GetEnvironmentVariable("MAPS_API_KEY");
+                    using HttpClient client = new HttpClient();
+                    foreach (Poilsiaviete poilsiaviete in Poilsiaviete)
+                    {
+                        string apiUrl = $"https://maps.googleapis.com/maps/api/geocode/json?address={Uri.EscapeDataString(poilsiaviete.Adresas)}&key={apiKey}";
+
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            JObject json = JObject.Parse(responseBody);
+                            if (json.HasValues)
+                            {
+                                string status = (string)json["status"];
+                                if (status == "OK")
+                                {
+                                    double platuma = (double)json["results"][0]["geometry"]["location"]["lat"];
+                                    double ilguma = (double)json["results"][0]["geometry"]["location"]["lng"];
+
+                                    locations.Add(new Tuple<double, double, string>(platuma, ilguma,
+                                        $"Pavadinimas: {poilsiaviete.Pavadinimas}<br>Adresas: {poilsiaviete.Adresas}"));
+                                }
+                            }
+                        }
+                        var serializedLocations = JsonConvert.SerializeObject(locations);
+                        TempData["SerializedLocations"] = serializedLocations;
+                    }
                 }
             }
         }
-
-        private async Task<List<Poilsiaviete>> FilterPoilsiavietes(DateOnly dateFrom, DateOnly dateTill)
+        public async Task<List<Poilsiaviete>> FilterPoilsiavietes(DateOnly dateFrom, DateOnly dateTill)
         {
             var Rezervacijos = await _context.Rezervacijos
                 .Where(r => r.Busena < 3)
